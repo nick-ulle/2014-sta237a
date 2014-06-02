77bf8eaafa8afb006bdc99e586e06b71cbcdcc58
Nick Ulle <nick.ulle@gmail.com>
2014-06-02 15:00:20 -0700

    Add Example 2 simulation.

--- src/final.R
+++ src/final.R
@@ -1,4 +1,9 @@
 #!/usr/bin/env RScript
+# Author: Nick Ulle
+# Description:
+#   Code for sta237a final project, based on [Dahlhaus & Janas, 1996]. 
+#   Included are spectral bootstrap methods, and code to reproduce both
+#   examples from the paper.
 
 main = function() {
 }
@@ -8,8 +13,8 @@ main = function() {
 #############
 
 example1 = function(seed = 506) {
-    # ----- Simulate Sample
     set.seed(seed)
+    # ----- Simulation
     n = 64
     a = 0.9
 
@@ -54,6 +59,54 @@ example1_spectral = function(x)
 {
     1 / (2 * pi * (1.81 - 1.8 * cos(x)))
 }
+
+#############
+# Example 2 #
+#############
+
+example2 = function(seed = 149) {
+    set.seed(seed)
+    # ----- Simulation
+    n = 64
+
+    # The AR coefficients, in reverse order.
+    ar = c(- 0.9^4, 
+           2 * 0.9^3 * (cos(pi/2) + cos(pi/5)), 
+           - 2 * 0.9^2 * (1 + 2 * cos(pi/2) * cos(pi/5)), 
+           2 * 0.9 * (cos(pi/2) + cos(pi/5))
+           )
+
+    # The MA coefficients, in reverse order, with 1 added for convenience.
+    ma = c(0.8^2, 
+           -2 * 0.8 * cos(0.35*pi), 
+           1)
+
+    # Sample uniform innovations.
+    # z_1 is z[3]
+    z = runif(n + 2, -sqrt(3), sqrt(3))
+
+    # Generate the observations.
+    # x_1 is x[5]
+    x = numeric(n + 4)
+    for (i in seq(1, n)) {
+        x[i + 4] = ar %*% x[i:(i + 3)] + ma %*% z[i:(i + 2)] 
+    }
+    # Discard x[1:4].
+    x = ts(x[-(1:4)])
+
+    # ----- Periodogram & Spectral Density
+    taper = tukey_hanning(n, 0.01)
+    I = periodogram(x, taper)
+
+    f_hat = kernel_smooth(I, 0.05, epanechnikov)
+
+    # ----- Bootstrap
+    # Set phi for computing Whittle estimates for AR(4) model.
+}
+
+###############
+# Application #
+###############
 
 #######################
 # Bootstrap Functions #
